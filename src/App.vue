2fc5593c906b60d5051fe3a1a8bc684077351ed5
HUNGNV10-D5\HungNV10 <hung.nv155790hust@gmail.com>
2023-09-29 15:40:35 +0700

    feat(i18n)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
+  <select v-model="locale">
+    <option>en</option>
+    <option>ja</option>
+  </select>
   <component :is="$route?.meta?.layout || 'div'"></component>
+  <div>{{ t("a") }}</div>
 </template>
-<script setup></script>
 
-<style scoped></style>
+<script>
+import { useI18n } from "vue-i18n";
+
+export default {
+  setup() {
+    const { t, locale } = useI18n();
+    return {
+      t,
+      locale,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
